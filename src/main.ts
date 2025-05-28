f265149d4eaffd2304ea4b835cf0599a4f69605e
iosmanthus <myosmanthustree@gmail.com>
2025-05-28 14:52:40 +0800

    uncordon dev stacks
    Signed-off-by: iosmanthus <myosmanthustree@gmail.com>

--- src/main.ts
+++ src/main.ts
@@ -36,9 +36,9 @@ const main = async () => {
     tableName: config.ddbLocksTable,
     ttl: config.ddbLocksTTL
   };
-  if (config.cloudUrl === "" && config.stackName.startsWith('dev')) {
-    throw new Error("Dev stacks are cordoned");
-  }
+  // if (config.cloudUrl === "" && config.stackName.startsWith('dev')) {
+  //   throw new Error("Dev stacks are cordoned");
+  // }
   const lockOwner = `${process.env.GITHUB_REPOSITORY}/${config.stackName}/${process.env.GITHUB_RUN_ID}`;
   const isPost = !!core.getState('isPost');
   if (!isPost) {
